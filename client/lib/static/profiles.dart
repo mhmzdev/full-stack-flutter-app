2f4a531a52473a65a0fc0123e131aae57fd4d8a0
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-27 21:49:31 +0500

    mock data for profiles

---
+++ client/lib/static/profiles.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:faker_dart/faker_dart.dart';
+import 'package:shared/shared.dart';
+
+final faker = Faker.instance;
+
+final _images = List.generate(6, (index) {
+  final i = index + 1;
+  final type = i.isEven ? 'boy' : 'girl';
+
+  final image = 'assets/pictures/$type-$i';
+  return image;
+});
+
+final profiles = List.generate(10, (index) {
+  final isMale = index.isEven;
+
+  final firstName =
+      faker.name.firstName(gender: isMale ? Gender.male : Gender.female);
+  final lastName =
+      faker.name.lastName(gender: isMale ? Gender.male : Gender.female);
+
+  final username = faker.lorem.word(length: 1);
+
+  final imageURL = _images[index];
+
+  final email = faker.internet.email();
+  final bio = faker.lorem.sentence(wordCount: 10);
+
+  final randomFollowersLen = Random().nextInt(10 + 1);
+  final followers = List.generate(
+    randomFollowersLen,
+    (index) => faker.datatype.number(min: 0, max: 10),
+  );
+
+  final randomFollowingLen = Random().nextInt(10 + 1);
+  final following = List.generate(
+    randomFollowingLen,
+    (index) => faker.datatype.number(min: 0, max: 10),
+  );
+
+  final randomPostsLen = Random().nextInt(10 + 1);
+  final posts = List.generate(
+    randomPostsLen,
+    (index) => faker.datatype.number(min: 0, max: 10),
+  );
+
+  return User(
+    id: index + 1,
+    firstName: firstName,
+    lastName: lastName,
+    username: username,
+    email: email,
+    bio: bio,
+    posts: posts,
+    followers: followers,
+    following: following,
+    imageURL: imageURL,
+    coverURL: '',
+  );
+});
