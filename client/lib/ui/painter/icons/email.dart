76f1a991bf16578bff75b31aa20169804325f0ba
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-28 20:48:34 +0500

    painter fixed

--- client/lib/ui/painter/icons/email.dart
+++ client/lib/ui/painter/icons/email.dart
@@ -9,108 +9,76 @@ class EmailIconPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     Path path_0 = Path();
-    path_0.moveTo(size.width * 0.3600000, size.height * 0.7916667);
-    path_0.lineTo(size.width * 0.3400000, size.height * 0.7916667);
+    path_0.moveTo(size.width * 0.7083333, size.height * 0.8541667);
+    path_0.lineTo(size.width * 0.2916667, size.height * 0.8541667);
     path_0.cubicTo(
-        size.width * 0.1800000,
+        size.width * 0.1666667,
+        size.height * 0.8541667,
+        size.width * 0.08333333,
         size.height * 0.7916667,
-        size.width * 0.1000000,
-        size.height * 0.7500000,
-        size.width * 0.1000000,
-        size.height * 0.5416667);
-    path_0.lineTo(size.width * 0.1000000, size.height * 0.3333333);
+        size.width * 0.08333333,
+        size.height * 0.6458333);
+    path_0.lineTo(size.width * 0.08333333, size.height * 0.3541667);
     path_0.cubicTo(
-        size.width * 0.1000000,
-        size.height * 0.1666667,
-        size.width * 0.1800000,
-        size.height * 0.08333333,
-        size.width * 0.3400000,
-        size.height * 0.08333333);
-    path_0.lineTo(size.width * 0.6600000, size.height * 0.08333333);
+        size.width * 0.08333333,
+        size.height * 0.2083333,
+        size.width * 0.1666667,
+        size.height * 0.1458333,
+        size.width * 0.2916667,
+        size.height * 0.1458333);
+    path_0.lineTo(size.width * 0.7083333, size.height * 0.1458333);
     path_0.cubicTo(
-        size.width * 0.8200000,
-        size.height * 0.08333333,
-        size.width * 0.9000000,
-        size.height * 0.1666667,
-        size.width * 0.9000000,
-        size.height * 0.3333333);
-    path_0.lineTo(size.width * 0.9000000, size.height * 0.5416667);
+        size.width * 0.8333333,
+        size.height * 0.1458333,
+        size.width * 0.9166667,
+        size.height * 0.2083333,
+        size.width * 0.9166667,
+        size.height * 0.3541667);
+    path_0.lineTo(size.width * 0.9166667, size.height * 0.6458333);
     path_0.cubicTo(
-        size.width * 0.9000000,
-        size.height * 0.7083333,
-        size.width * 0.8200000,
+        size.width * 0.9166667,
         size.height * 0.7916667,
-        size.width * 0.6600000,
-        size.height * 0.7916667);
-    path_0.lineTo(size.width * 0.6400000, size.height * 0.7916667);
-    path_0.cubicTo(
-        size.width * 0.6276000,
-        size.height * 0.7916667,
-        size.width * 0.6156000,
-        size.height * 0.7979167,
-        size.width * 0.6080000,
-        size.height * 0.8083333);
-    path_0.lineTo(size.width * 0.5480000, size.height * 0.8916667);
-    path_0.cubicTo(
-        size.width * 0.5216000,
-        size.height * 0.9283333,
-        size.width * 0.4784000,
-        size.height * 0.9283333,
-        size.width * 0.4520000,
-        size.height * 0.8916667);
-    path_0.lineTo(size.width * 0.3920000, size.height * 0.8083333);
-    path_0.cubicTo(
-        size.width * 0.3856000,
-        size.height * 0.7991667,
-        size.width * 0.3708000,
-        size.height * 0.7916667,
-        size.width * 0.3600000,
-        size.height * 0.7916667);
+        size.width * 0.8333333,
+        size.height * 0.8541667,
+        size.width * 0.7083333,
+        size.height * 0.8541667);
     path_0.close();
 
     Paint paint0Stroke = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = size.width * 0.06000000;
-    paint0Stroke.color = Colors.white.withOpacity(1.0);
+      ..strokeWidth = size.width * 0.06250000;
+    paint0Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
     paint0Stroke.strokeCap = StrokeCap.round;
     paint0Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_0, paint0Stroke);
 
     Paint paint0Fill = Paint()..style = PaintingStyle.fill;
-    paint0Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint0Fill.color = Colors.transparent;
     canvas.drawPath(path_0, paint0Fill);
 
     Path path_1 = Path();
-    path_1.moveTo(size.width * 0.3000000, size.height * 0.3333333);
-    path_1.lineTo(size.width * 0.7000000, size.height * 0.3333333);
+    path_1.moveTo(size.width * 0.7083333, size.height * 0.3750000);
+    path_1.lineTo(size.width * 0.5779167, size.height * 0.4791667);
+    path_1.cubicTo(
+        size.width * 0.5350000,
+        size.height * 0.5133333,
+        size.width * 0.4645833,
+        size.height * 0.5133333,
+        size.width * 0.4216667,
+        size.height * 0.4791667);
+    path_1.lineTo(size.width * 0.2916667, size.height * 0.3750000);
 
     Paint paint1Stroke = Paint()
       ..style = PaintingStyle.stroke
-      ..strokeWidth = size.width * 0.06000000;
-    paint1Stroke.color = Colors.white.withOpacity(1.0);
+      ..strokeWidth = size.width * 0.06250000;
+    paint1Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
     paint1Stroke.strokeCap = StrokeCap.round;
     paint1Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_1, paint1Stroke);
 
     Paint paint1Fill = Paint()..style = PaintingStyle.fill;
-    paint1Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint1Fill.color = Colors.transparent;
     canvas.drawPath(path_1, paint1Fill);
-
-    Path path_2 = Path();
-    path_2.moveTo(size.width * 0.3000000, size.height * 0.5416667);
-    path_2.lineTo(size.width * 0.5400000, size.height * 0.5416667);
-
-    Paint paint2Stroke = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = size.width * 0.06000000;
-    paint2Stroke.color = Colors.white.withOpacity(1.0);
-    paint2Stroke.strokeCap = StrokeCap.round;
-    paint2Stroke.strokeJoin = StrokeJoin.round;
-    canvas.drawPath(path_2, paint2Stroke);
-
-    Paint paint2Fill = Paint()..style = PaintingStyle.fill;
-    paint2Fill.color = const Color(0xff000000).withOpacity(1.0);
-    canvas.drawPath(path_2, paint2Fill);
   }
 
   @override
