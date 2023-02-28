4ad2bf102fb2386a5d6b359f5a0b93e33c5d44d7
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-28 21:47:48 +0500

    app back button added

---
+++ client/lib/ui/widgets/design/buttons/app_back_button.dart
@@ -0,0 +1,30 @@
+import 'package:client/configs/configs.dart';
+import 'package:client/ui/painter/icons/arrow_left.dart';
+import 'package:flutter/material.dart';
+
+class AppBackButton extends StatelessWidget {
+  final void Function()? onTap;
+  const AppBackButton({super.key, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => onTap ?? Navigator.pop(context),
+      child: Container(
+        padding: Space.a.t15,
+        decoration: const BoxDecoration(
+          shape: BoxShape.circle,
+          color: AppTheme.backgroundLight,
+        ),
+        child: Center(
+          child: CustomPaint(
+            painter: const ArrowLeftIconPainter(
+              color: AppTheme.grey,
+            ),
+            size: ArrowLeftIconPainter.s(12.un()),
+          ),
+        ),
+      ),
+    );
+  }
+}
