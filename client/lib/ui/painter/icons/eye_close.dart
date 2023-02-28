704e5adea49980b623438ff357be3c5565a15838
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-28 15:50:43 +0500

    painter color fixes

--- client/lib/ui/painter/icons/eye_close.dart
+++ client/lib/ui/painter/icons/eye_close.dart
@@ -37,13 +37,13 @@ class EyeCloseIconPainter extends CustomPainter {
     Paint paint0Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint0Stroke.color = const Color(0xff292D32).withOpacity(1.0);
+    paint0Stroke.color = color ?? const Color(0xff292D32).withOpacity(1.0);
     paint0Stroke.strokeCap = StrokeCap.round;
     paint0Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_0, paint0Stroke);
 
     Paint paint0Fill = Paint()..style = PaintingStyle.fill;
-    paint0Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint0Fill.color = Colors.transparent;
     canvas.drawPath(path_0, paint0Fill);
 
     Path path_1 = Path();
@@ -80,13 +80,13 @@ class EyeCloseIconPainter extends CustomPainter {
     Paint paint1Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint1Stroke.color = const Color(0xff292D32).withOpacity(1.0);
+    paint1Stroke.color = color ?? const Color(0xff292D32).withOpacity(1.0);
     paint1Stroke.strokeCap = StrokeCap.round;
     paint1Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_1, paint1Stroke);
 
     Paint paint1Fill = Paint()..style = PaintingStyle.fill;
-    paint1Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint1Fill.color = Colors.transparent;
     canvas.drawPath(path_1, paint1Fill);
 
     Path path_2 = Path();
@@ -123,13 +123,13 @@ class EyeCloseIconPainter extends CustomPainter {
     Paint paint2Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint2Stroke.color = const Color(0xff292D32).withOpacity(1.0);
+    paint2Stroke.color = color ?? const Color(0xff292D32).withOpacity(1.0);
     paint2Stroke.strokeCap = StrokeCap.round;
     paint2Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_2, paint2Stroke);
 
     Paint paint2Fill = Paint()..style = PaintingStyle.fill;
-    paint2Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint2Fill.color = Colors.transparent;
     canvas.drawPath(path_2, paint2Fill);
 
     Path path_3 = Path();
@@ -145,13 +145,13 @@ class EyeCloseIconPainter extends CustomPainter {
     Paint paint3Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint3Stroke.color = const Color(0xff292D32).withOpacity(1.0);
+    paint3Stroke.color = color ?? const Color(0xff292D32).withOpacity(1.0);
     paint3Stroke.strokeCap = StrokeCap.round;
     paint3Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_3, paint3Stroke);
 
     Paint paint3Fill = Paint()..style = PaintingStyle.fill;
-    paint3Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint3Fill.color = color ?? const Color(0xff000000).withOpacity(1.0);
     canvas.drawPath(path_3, paint3Fill);
 
     Path path_4 = Path();
@@ -161,13 +161,13 @@ class EyeCloseIconPainter extends CustomPainter {
     Paint paint4Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint4Stroke.color = const Color(0xff292D32).withOpacity(1.0);
+    paint4Stroke.color = color ?? const Color(0xff292D32).withOpacity(1.0);
     paint4Stroke.strokeCap = StrokeCap.round;
     paint4Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_4, paint4Stroke);
 
     Paint paint4Fill = Paint()..style = PaintingStyle.fill;
-    paint4Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint4Fill.color = color ?? const Color(0xff000000).withOpacity(1.0);
     canvas.drawPath(path_4, paint4Fill);
 
     Path path_5 = Path();
@@ -177,13 +177,13 @@ class EyeCloseIconPainter extends CustomPainter {
     Paint paint5Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint5Stroke.color = const Color(0xff292D32).withOpacity(1.0);
+    paint5Stroke.color = color ?? const Color(0xff292D32).withOpacity(1.0);
     paint5Stroke.strokeCap = StrokeCap.round;
     paint5Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_5, paint5Stroke);
 
     Paint paint5Fill = Paint()..style = PaintingStyle.fill;
-    paint5Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint5Fill.color = color ?? const Color(0xff000000).withOpacity(1.0);
     canvas.drawPath(path_5, paint5Fill);
   }
 
--- client/lib/ui/painter/icons/lock.dart
+++ client/lib/ui/painter/icons/lock.dart
@@ -30,13 +30,13 @@ class LockIconPainter extends CustomPainter {
     Paint paint0Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint0Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
+    paint0Stroke.color = color ?? const Color(0xffABABAB).withOpacity(1.0);
     paint0Stroke.strokeCap = StrokeCap.round;
     paint0Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_0, paint0Stroke);
 
     Paint paint0Fill = Paint()..style = PaintingStyle.fill;
-    paint0Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint0Fill.color = Colors.transparent;
     canvas.drawPath(path_0, paint0Fill);
 
     Path path_1 = Path();
@@ -78,13 +78,13 @@ class LockIconPainter extends CustomPainter {
     Paint paint1Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.06250000;
-    paint1Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
+    paint1Stroke.color = color ?? const Color(0xffABABAB).withOpacity(1.0);
     paint1Stroke.strokeCap = StrokeCap.round;
     paint1Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_1, paint1Stroke);
 
     Paint paint1Fill = Paint()..style = PaintingStyle.fill;
-    paint1Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint1Fill.color = Colors.transparent;
     canvas.drawPath(path_1, paint1Fill);
 
     Path path_2 = Path();
@@ -94,13 +94,13 @@ class LockIconPainter extends CustomPainter {
     Paint paint2Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.08333333;
-    paint2Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
+    paint2Stroke.color = color ?? const Color(0xffABABAB).withOpacity(1.0);
     paint2Stroke.strokeCap = StrokeCap.round;
     paint2Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_2, paint2Stroke);
 
     Paint paint2Fill = Paint()..style = PaintingStyle.fill;
-    paint2Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint2Fill.color = Colors.transparent;
     canvas.drawPath(path_2, paint2Fill);
 
     Path path_3 = Path();
@@ -110,13 +110,13 @@ class LockIconPainter extends CustomPainter {
     Paint paint3Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.08333333;
-    paint3Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
+    paint3Stroke.color = color ?? const Color(0xffABABAB).withOpacity(1.0);
     paint3Stroke.strokeCap = StrokeCap.round;
     paint3Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_3, paint3Stroke);
 
     Paint paint3Fill = Paint()..style = PaintingStyle.fill;
-    paint3Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint3Fill.color = Colors.transparent;
     canvas.drawPath(path_3, paint3Fill);
 
     Path path_4 = Path();
@@ -126,13 +126,13 @@ class LockIconPainter extends CustomPainter {
     Paint paint4Stroke = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = size.width * 0.08333333;
-    paint4Stroke.color = const Color(0xffABABAB).withOpacity(1.0);
+    paint4Stroke.color = color ?? const Color(0xffABABAB).withOpacity(1.0);
     paint4Stroke.strokeCap = StrokeCap.round;
     paint4Stroke.strokeJoin = StrokeJoin.round;
     canvas.drawPath(path_4, paint4Stroke);
 
     Paint paint4Fill = Paint()..style = PaintingStyle.fill;
-    paint4Fill.color = const Color(0xff000000).withOpacity(1.0);
+    paint4Fill.color = Colors.transparent;
     canvas.drawPath(path_4, paint4Fill);
   }
 
