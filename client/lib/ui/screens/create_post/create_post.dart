788786fd07368e2781cfb03f6108a0221e5fa214
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-01 12:18:16 +0500

    create post screen

--- client/lib/ui/screens/create_post/create_post.dart
+++ client/lib/ui/screens/create_post/create_post.dart
@@ -1,5 +1,13 @@
 import 'package:client/configs/configs.dart';
+import 'package:client/static/profiles.dart';
+import 'package:client/ui/painter/icons/camera.dart';
+import 'package:client/ui/painter/icons/gallery.dart';
 import 'package:client/ui/widgets/core/screen/screen.dart';
+import 'package:client/ui/widgets/design/avatar/avatar.dart';
+import 'package:client/ui/widgets/design/button/button.dart';
+import 'package:client/ui/widgets/design/buttons/app_back_button.dart';
+import 'package:client/ui/widgets/design/input/app_multiline_input.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -10,6 +18,7 @@ part 'static/_form_data.dart';
 part 'static/_form_keys.dart';
 
 part 'widgets/_body.dart';
+part 'widgets/_modal.dart';
 
 part '_state.dart';
 
--- client/lib/ui/screens/create_post/widgets/_body.dart
+++ client/lib/ui/screens/create_post/widgets/_body.dart
@@ -12,12 +12,72 @@ class _Body extends StatelessWidget {
       formKey: screenState.formKey,
       initialFormValue: _FormData.initialValues(),
       child: SafeArea(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            //
-          ],
+        child: SingleChildScrollView(
+          padding: Space.a.t25,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: [
+                  const AppBackButton(),
+                  Space.x.t15,
+                  Text(
+                    'Create Post',
+                    style: AppText.b1,
+                  )
+                ],
+              ),
+              Space.y.t60,
+              Avatar(
+                user: profiles.first,
+                type: AvatarType.detailed,
+              ),
+              Space.y.t30,
+              const AppMultilineInputField(
+                name: _FormKeys.caption,
+                hint: 'Start typing here...',
+                maxLines: 10,
+              ),
+              Space.y.t60,
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        backgroundColor: Colors.transparent,
+                        builder: (_) => const _Modal(),
+                      );
+                    },
+                    child: DottedBorder(
+                      borderType: BorderType.RRect,
+                      radius: const Radius.circular(12),
+                      strokeWidth: 2,
+                      dashPattern: const [10],
+                      color: AppTheme.grey,
+                      child: SizedBox(
+                        height: 65.un(),
+                        width: 50.un(),
+                        child: Center(
+                          child: CustomPaint(
+                            painter: const GalleryIconPainter(),
+                            size: GalleryIconPainter.s(
+                              15.un(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Space.y.t60,
+              AppButton(
+                label: 'POST',
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
