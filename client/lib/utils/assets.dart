d25c144c76d7997df469e4a9bb4d96bb943f72a2
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-01 11:39:27 +0500

    home screen completed with empty feed

--- client/lib/utils/assets.dart
+++ client/lib/utils/assets.dart
@@ -4,4 +4,8 @@ class StaticAssets {
   //
   static const emptyFeed = 'assets/illustrations/empty-feed.png';
   static const forgotPassword = 'assets/illustrations/forgot-password.png';
+
+  //
+  static const arrowWhirlUp = 'assets/utils/arrow-whirl-up.png';
+  static const arrowWhirlDown = 'assets/utils/arrow-whirl-down.png';
 }
