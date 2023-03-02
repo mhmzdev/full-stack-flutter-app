3513a75cfed60051b05349a4b61f6cc1e6a6cfd9
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-02 10:30:49 +0500

    register and login routes added

---
+++ server/routes/api/v1/users/login.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return Response.json(
+        body: {
+          'message': context.request.uri.path,
+        },
+      );
+    case HttpMethod.post:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.put:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.delete:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.patch:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.head:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.options:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
