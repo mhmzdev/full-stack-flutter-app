5d0ed9c8275ed34ae6348472c638e22a68a5be18
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-02 09:58:45 +0500

    extra middlewards removed

--- server/routes/api/v1/comments/_middleware.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-import 'package:stormberry/stormberry.dart';
-import '../../../_middleware.dart';
-
-Handler middleware(Handler handler) {
-  return handler.use(provider<Database>((_) => database));
-}
