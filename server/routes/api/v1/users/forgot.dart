e37a393f0ab698e43bfea7a280be879f1a943481
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-02 20:40:33 +0500

    forgot password route

---
+++ server/routes/api/v1/users/forgot.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:db/db.dart' as db;
+import 'package:stormberry/stormberry.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.post:
+      final req = await context.request.body();
+      final map = jsonDecode(req) as Map<String, dynamic>;
+      return _forgotPassword(context, map['email'] as String);
+
+    case HttpMethod.put:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.delete:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.patch:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.head:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.options:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _forgotPassword(RequestContext context, String email) async {
+  final database = context.read<Database>();
+  final param = QueryParams(
+    where: 'email',
+    values: {
+      'email': email,
+    },
+  );
+  final users = await database.users.queryUsers(param);
+  if (users.isEmpty) {
+    return Response.json(
+      statusCode: 205,
+      body: {
+        'status': 'failure',
+        'message': 'Email not found. Please try again!',
+      },
+    );
+  }
+
+  // TODO: Sent reset link to email
+  return Response.json(
+    body: {
+      'status': 'failure',
+      'message': 'Please follow the instructions sent at your email $email',
+    },
+  );
+}
