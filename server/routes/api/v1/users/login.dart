98339c995995f9fe3c7e7f939dbaa9137a5935e9
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-02 19:56:07 +0500

    login and register calls added

--- server/routes/api/v1/users/login.dart
+++ server/routes/api/v1/users/login.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:crypt/crypt.dart';
 import 'package:dart_frog/dart_frog.dart';
+import 'package:db/db.dart' as db;
+import 'package:shared/shared.dart';
+import 'package:stormberry/stormberry.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   switch (context.request.method) {
     case HttpMethod.get:
-      return Response.json(
-        body: {
-          'message': context.request.uri.path,
-        },
-      );
+      final request = await context.request.body();
+      final map = jsonDecode(request) as Map<String, dynamic>;
+      return _login(context, map);
+
     case HttpMethod.post:
       return Response(statusCode: HttpStatus.methodNotAllowed);
     case HttpMethod.put:
@@ -23,4 +27,53 @@ Future<Response> onRequest(RequestContext context) async {
     case HttpMethod.options:
       return Response(statusCode: HttpStatus.methodNotAllowed);
   }
+}
+
+Future<Response> _login(
+  RequestContext context,
+  Map<String, dynamic> body,
+) async {
+  db.User? user;
+
+  final database = context.read<Database>();
+  final users = await database.users.queryUsers();
+
+  for (final u in users) {
+    if (u.email == body['email']) {
+      user = u;
+    }
+  }
+
+  if (user == null) {
+    return Response.json(
+      statusCode: 205,
+      body: {
+        'status': 'failure',
+        'message': "Account doesn't exist for this email. Please try again!",
+      },
+    );
+  }
+
+  final hashedPassword = user.password;
+  final password = body['password'] as String;
+  final doMatch = Crypt(hashedPassword).match(password);
+
+  if (!doMatch) {
+    return Response.json(
+      statusCode: 203,
+      body: {
+        'status': 'failure',
+        'message': 'Email/Password do not match!',
+      },
+    );
+  }
+
+  return Response.json(
+    body: {
+      'status': 'Success',
+      body: {
+        'user': User.fromDb(user),
+      },
+    },
+  );
 }
--- server/routes/api/v1/users/register.dart
+++ server/routes/api/v1/users/register.dart
@@ -1,16 +1,19 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:crypt/crypt.dart';
 import 'package:dart_frog/dart_frog.dart';
+import 'package:db/db.dart' as db;
+import 'package:stormberry/stormberry.dart';
 
 Future<Response> onRequest(RequestContext context) async {
   switch (context.request.method) {
-    case HttpMethod.get:
-      return Response.json(
-        body: {
-          'message': context.request.uri.path,
-        },
-      );
     case HttpMethod.post:
+      final request = await context.request.body();
+      final map = jsonDecode(request) as Map<String, dynamic>;
+      return _register(context, map);
+
+    case HttpMethod.get:
       return Response(statusCode: HttpStatus.methodNotAllowed);
     case HttpMethod.put:
       return Response(statusCode: HttpStatus.methodNotAllowed);
@@ -23,4 +26,49 @@ Future<Response> onRequest(RequestContext context) async {
     case HttpMethod.options:
       return Response(statusCode: HttpStatus.methodNotAllowed);
   }
+}
+
+Future<Response> _register(
+  RequestContext context,
+  Map<String, dynamic> body,
+) async {
+  final database = context.read<Database>();
+  final users = await database.users.queryUsers();
+
+  for (final u in users) {
+    if (u.username == body['username']) {
+      return Response.json(
+        statusCode: 205,
+        body: {
+          'status': 'failed',
+          'message': 'username ${body['username']} is already taken!',
+        },
+      );
+    }
+  }
+
+  final hashedPassword = Crypt.sha256(body['password'] as String);
+
+  final request = db.UserInsertRequest(
+    firstName: body['firstName'] as String,
+    lastName: body['lastName'] as String,
+    username: body['username'] as String,
+    email: body['email'] as String,
+    password: hashedPassword.toString(),
+    imageURL: '',
+    coverURL: '',
+    bio: '',
+    followers: [],
+    following: [],
+    posts: [],
+  );
+
+  final id = await database.users.insertOne(request);
+
+  return Response.json(
+    body: {
+      'status': 'success',
+      'message': 'User has been added with ID: $id',
+    },
+  );
 }
