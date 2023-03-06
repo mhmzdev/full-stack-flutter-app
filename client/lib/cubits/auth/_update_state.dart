03d2ba9a3974c3a7dc3e0be46cb63bf521db73b4
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-06 22:44:06 +0500

    update states added

---
+++ client/lib/cubits/auth/_update_state.dart
@@ -0,0 +1,33 @@
+part of 'cubit.dart';
+
+@immutable
+class AuthUpdateState extends Equatable {
+  static bool match(AuthState a, AuthState b) => a.update != b.update;
+
+  final String? message;
+
+  const AuthUpdateState({
+    this.message,
+  });
+
+  @override
+  List<Object?> get props => [
+        message,
+      ];
+}
+
+@immutable
+class AuthUpdateDefault extends AuthUpdateState {}
+
+@immutable
+class AuthUpdateLoading extends AuthUpdateState {}
+
+@immutable
+class AuthUpdateSuccess extends AuthUpdateState {
+  const AuthUpdateSuccess() : super();
+}
+
+@immutable
+class AuthUpdateFailed extends AuthUpdateState {
+  const AuthUpdateFailed({String? message}) : super(message: message);
+}
