0ed99406a98b8983800b7c488db12d1529ed2070
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-06 14:30:13 +0500

    states separated

---
+++ client/lib/cubits/auth/_login_state.dart
@@ -0,0 +1,33 @@
+part of 'cubit.dart';
+
+@immutable
+class AuthLoginState extends Equatable {
+  static bool match(AuthState a, AuthState b) => a.login != b.login;
+
+  final String? message;
+
+  const AuthLoginState({
+    this.message,
+  });
+
+  @override
+  List<Object?> get props => [
+        message,
+      ];
+}
+
+@immutable
+class AuthLoginDefault extends AuthLoginState {}
+
+@immutable
+class AuthLoginLoading extends AuthLoginState {}
+
+@immutable
+class AuthLoginSuccess extends AuthLoginState {
+  const AuthLoginSuccess() : super();
+}
+
+@immutable
+class AuthLoginFailed extends AuthLoginState {
+  const AuthLoginFailed({String? message}) : super(message: message);
+}
---
+++ client/lib/cubits/auth/_register_state.dart
@@ -0,0 +1,33 @@
+part of 'cubit.dart';
+
+@immutable
+class AuthRegisterState extends Equatable {
+  static bool match(AuthState a, AuthState b) => a.register != b.register;
+
+  final String? message;
+
+  const AuthRegisterState({
+    this.message,
+  });
+
+  @override
+  List<Object?> get props => [
+        message,
+      ];
+}
+
+@immutable
+class AuthRegisterDefault extends AuthRegisterState {}
+
+@immutable
+class AuthRegisterLoading extends AuthRegisterState {}
+
+@immutable
+class AuthRegisterSuccess extends AuthRegisterState {
+  const AuthRegisterSuccess() : super();
+}
+
+@immutable
+class AuthRegisterFailed extends AuthRegisterState {
+  const AuthRegisterFailed({String? message}) : super(message: message);
+}
