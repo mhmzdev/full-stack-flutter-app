6951ec2daeee6468c5c27a8cb437d7b989940fb1
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-06 12:15:56 +0500

    exception added

--- client/lib/cubits/auth/data_provider.dart
+++ client/lib/cubits/auth/data_provider.dart
@@ -6,6 +6,8 @@ class _AuthProvider {
       final Map<String, dynamic> raw = {};
       return User.fromJson(raw);
     } catch (e) {
+      debugPrint('------ AuthProvider ------');
+      debugPrint('------ $e ------');
       throw Exception("Internal Server Error");
     }
   }
