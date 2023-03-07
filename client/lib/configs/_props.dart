937e40a3b3a6b7736398b3a10f927969e624b918
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-03-07 15:13:32 +0500

    modal decoration extracted

--- client/lib/configs/_props.dart
+++ client/lib/configs/_props.dart
@@ -2,4 +2,11 @@ part of 'configs.dart';
 
 abstract class AppProps {
   static final duration = 320.milliseconds;
+
+  static final modalDec = BoxDecoration(
+    color: AppTheme.backgroundDark,
+    borderRadius: BorderRadius.vertical(
+      top: Radius.circular(6.un()),
+    ),
+  );
 }
