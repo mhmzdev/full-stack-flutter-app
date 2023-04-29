01295f5ab1bcdafd56cd79c3d195082fef99d65e
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-04-29 11:42:35 +0500

    story routes added CRUD

---
+++ server/routes/api/v1/stories/[id].dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:db/db.dart' as db;
+import 'package:shared/shared.dart';
+import 'package:stormberry/stormberry.dart';
+
+Future<Response> onRequest(RequestContext context, String stringId) async {
+  final id = int.parse(stringId);
+  final database = context.read<Database>();
+  final dbStory = await database.stories.queryStory(id);
+
+  if (dbStory == null) {
+    return Response.json(
+      body: {
+        'status': 'failure',
+        'message': 'Story not found',
+      },
+      statusCode: HttpStatus.notFound,
+    );
+  }
+
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context, dbStory);
+    case HttpMethod.delete:
+      return _delete(context, id, dbStory);
+
+    //
+    case HttpMethod.put:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.post:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.patch:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.head:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.options:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _get(RequestContext context, db.StoryView dbStory) async {
+  final shareStory = Story.fromStoryView(dbStory);
+
+  return Response.json(
+    body: shareStory.toJson(),
+  );
+}
+
+Future<Response> _delete(
+  RequestContext context,
+  int id,
+  db.StoryView story,
+) async {
+  final database = context.read<Database>();
+  final user = await database.users.queryUser(story.uid);
+  user!.stories.remove(id);
+
+  final updateUserReq = db.UserUpdateRequest(
+    id: story.uid,
+    stories: user.stories,
+  );
+
+  await database.stories.deleteOne(id);
+  await database.users.updateOne(updateUserReq);
+
+  return Response.json(
+    body: {
+      'status': 'success',
+      'message': 'Story has been deleted successfully!',
+    },
+  );
+}
---
+++ server/routes/api/v1/stories/index.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:db/db.dart' as db;
+import 'package:shared/shared.dart';
+import 'package:stormberry/stormberry.dart';
+
+Future<Response> onRequest(RequestContext context) async {
+  switch (context.request.method) {
+    case HttpMethod.get:
+      return _get(context);
+    case HttpMethod.post:
+      final request = await context.request.body();
+      final map = jsonDecode(request) as Map<String, dynamic>;
+      return _post(context, map);
+
+    //
+    case HttpMethod.put:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.delete:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.patch:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.head:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+    case HttpMethod.options:
+      return Response(statusCode: HttpStatus.methodNotAllowed);
+  }
+}
+
+Future<Response> _get(RequestContext context) async {
+  final db = context.read<Database>();
+  final dbStories = await db.stories.queryStorys();
+
+  var sharedStories = <Story>[];
+
+  if (dbStories.isNotEmpty) {
+    sharedStories = dbStories.map(Story.fromStoryView).toList();
+  }
+
+  return Response.json(
+    body: {
+      'status': 'success',
+      'data': sharedStories,
+    },
+  );
+}
+
+Future<Response> _post(
+  RequestContext context,
+  Map<String, dynamic> body,
+) async {
+  final database = context.read<Database>();
+  final uid = body['uid'] as int;
+
+  final request = db.StoryInsertRequest(
+    uid: uid,
+    hasImage: body['hasImage'] as bool?,
+    imageUrl: body['imageURL'] as String?,
+    hasVideo: body['hasVideo'] as bool?,
+    videoUrl: body['videoURL'] as String?,
+    createdAt: DateTime.now(),
+  );
+
+  final storyId = await database.stories.insertOne(request);
+  final user = await database.users.queryUser(uid);
+  final userRequest = db.UserUpdateRequest(
+    id: uid,
+    stories: [...user!.stories, storyId],
+  );
+  await database.users.updateOne(userRequest);
+
+  final dbStory = await database.stories.queryStory(storyId);
+  final sharedStory = Story.fromStoryView(dbStory!);
+
+  return Response.json(
+    body: {
+      'status': 'success',
+      'data': sharedStory.toJson(),
+    },
+  );
+}
