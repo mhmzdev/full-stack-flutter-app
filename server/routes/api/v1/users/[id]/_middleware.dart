9c6d78d97b2ba4aca0f15c904a1f0b8dbfbfb58a
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2025-05-08 12:04:43 +0500

    authentication added on users route specific

---
+++ server/routes/api/v1/users/[id]/_middleware.dart
@@ -0,0 +1,23 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:dart_frog_auth/dart_frog_auth.dart';
+import 'package:stormberry/stormberry.dart';
+
+import '../../_middleware.dart';
+import '../../authenticator.dart';
+
+Handler middleware(Handler handler) {
+  final authy = Authenticator(database: database);
+  return handler
+      .use(
+        provider<Database>((context) {
+          return database;
+        }),
+      )
+      .use(
+        bearerAuthentication<bool>(
+          authenticator: (context, token) {
+            return authy.verifyToken(token);
+          },
+        ),
+      );
+}
